b988ca885aa95494bd29e23691b2e069c00410e6
Kolton Guthrie <guthriekolton@gmail.com>
2025-03-05 02:05:09 -0600

    Fixed redirect error in dashboard router ('/')

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('/users');
+    res.redirect('dashboard/users');
 });
 
 router.get('/users', (req, res) => {
