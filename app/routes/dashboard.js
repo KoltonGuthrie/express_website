2f6614c85fa965b8f7a21a782d141a9e0ff5e350
Kolton Guthrie <guthriekolton@gmail.com>
2025-03-05 17:41:08 -0600

    Added middleware to dashboard route to get data

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,5 +1,30 @@
 import express from "express";
+import { getCredentialsByUsername } from '../database/credentials.js'
+import { getUserRoleById } from '../database/roles.js'
+import { getUserById } from '../database/user.js'
 const router = express.Router();
+
+router.use(async (req, res, next) => {
+    const username = req.session.username;
+    if(!username) return res.send("Internal server error").status(500);
+
+    const data = {};
+
+    const creds = await getCredentialsByUsername(username);
+    if(!creds?.user_id) return res.send("Internal server error").status(500);
+    data.username = creds.username;
+    data.displayname = creds.displayname;
+
+    const user = await getUserById(creds.user_id);
+    data.email = user.email;
+
+    const role = await getUserRoleById(creds.user_id);
+    data.role = role.name;
+
+    res.locals.currentUser = data;
+
+    next();
+});
 
 router.get("/", (req, res) => {
   res.redirect("dashboard/users");
@@ -8,7 +33,6 @@ router.get("/", (req, res) => {
 router.get("/users", (req, res) => {
   res.render("dashboard/users", {
     layout: "dashboard/layout",
-    currentUser: { username: "kolton", role: "admin" },
     users: [{ id: 1, username: "test" }],
   });
 });
@@ -16,21 +40,18 @@ router.get("/users", (req, res) => {
 router.get("/settings", (req, res) => {
   res.render("dashboard/settings", {
     layout: "dashboard/layout",
-    currentUser: { username: "kolton", role: "admin" },
   });
 });
 
 router.get("/reports", (req, res) => {
   res.render("dashboard/reports", {
     layout: "dashboard/layout",
-    currentUser: { username: "kolton", role: "admin" },
   });
 });
 
 router.get("/profile", (req, res) => {
   res.render("dashboard/profile", {
     layout: "dashboard/layout",
-    currentUser: { username: "kolton", role: "admin" },
   });
 });
 
