609859389b6cedfa88fc9b8fe4034f864c20b29c
Kolton Guthrie <guthriekolton@gmail.com>
2025-03-04 22:20:05 -0600

    Updated login/logout

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import express from 'express';
 import session from 'express-session';
+import { isLoggedIn } from './utils.js'
+import { isValidCredentials } from './database/credentials.js';
 
 const app = express();
 
@@ -22,6 +24,29 @@ app.set('views', './app/pages');
 
 app.get('/', (req, res) => {
     res.send("Hello, World!");
+});
+
+app.get('/login', (req, res) => {
+    if (isLoggedIn(req))
+        return res.redirect('/');
+
+    res.render('login/index');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (isValidCredentials(username, password)) {
+        req.session.username = username;
+        res.send('Login successful');
+    } else {
+        res.send('Invalid credentials');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.send("Logged out.");
 });
 
 export default app;
